d9313f4c81c389e1d154c4edecabcf3bdd7e33dc
nguyen.quang.phu <SUN-ASTERISK\nguyen.quang.phu@i120851-pc.sun-asterisk.com>
2020-03-27 13:45:23 +0700

    first commit for this package

---
+++ src/SayHelloByPhu.php
@@ -0,0 +1,9 @@
+<?php
+  namespace quangphu\SayHelloByPhu;
+
+  class SayHelloByPhu {
+    public static function hello_world() {
+      return "Xin chao ta ca moi nguoi, minh la Nguyen Quang Phu";
+    }
+  }
+?>
